e8105017a24e6b39aef1bd6f09079007c6e20543
dev <105196@ondirect.in>
2026-06-26 18:44:22 +0530

    Initial commit: First React Project

---
+++ React-blog/src/UserComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login Page</div>
+  )
+}
+
+export default Login
+
+
+export const ContactUs = () => {
+  return (
+    <div>ContactUs</div>
+  )
+}
+
